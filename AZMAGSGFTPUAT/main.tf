0a965533d1d4c25082f0ea44c2df45a3d50d5db6
bpourian <b.pourian@hotmail.co.uk>
2018-07-06 10:04:47 +0100

    Updated PUAT resource group with vnet and ubnets tests passing

--- AZMAGSGFTPUAT/main.tf
+++ AZMAGSGFTPUAT/main.tf
@@ -6,52 +6,28 @@
 
 provider "azurerm" {
   subscription_id = "${var.subscription_id}"
-  tenant_id       = "${var.tenant_id}"
 }
 
-resource "azurerm_resource_group" "AZMAGRGFTPUAT" {
-  name     = "${var.azurerm_resource_group}"
-  location = "${var.location}"
+resource "azurerm_resource_group" "machine" {
+    name     = "${var.azurerm_resource_group_name_machine}"
+    location = "${var.location}"
 }
 
-resource "azurerm_virtual_network" "AZMAGVNET1" {
-  name                = "AZMAGVNET1"
-  resource_group_name = "AZMAGRGVNET1"
+resource "azurerm_resource_group" "vnetrm" {
+    name     = "${var.azurerm_resource_group_name_vnet}"
+    location = "${var.location}"
 }
 
-resource "azurerm_subnet" "AZMAGSNET11" {
-  name                 = "${var.azurerm_subnet}"
-  virtual_network_name = "${var.azurerm_virtual_network.AZMAGVNET1.name}"
+resource "azurerm_virtual_network" "vnet" {
+    name                = "AZMAGVNET1"
+    address_space       = ["${var.vnet_cidr}"]
+    location            = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.vnetrm.name}"
 }
 
-resource "azurerm_virtual_machine" "AZMAGVMCFTPUAT" {
-  name                  = "AZMAGSGFTPUAT"
-  location              = "${var.location}"
-  resource_group_name   = "${azurerm_resource_group}"
-  # network_interface_ids = ["${azurerm_network_interface.public_nic.id}"]
-  vm_size               = "Standard_DS1_v2"
-
-  #This will delete the OS disk and data disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
-
-  storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "${var.azurerm_resource_group}-OSdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "windows"
-    admin_username = "${var.admin_username}"
-    admin_password = "${var.admin_password}"
-
-  }
+resource "azurerm_subnet" "vnetsub1" {
+    name                 = "AZMAGSNET11"
+    address_prefix       = "${var.subnet1_cidr}"
+    resource_group_name  = "${azurerm_resource_group.vnetrm.name}"
+    virtual_network_name = "${azurerm_virtual_network.vnet.name}"
 }
--- AZMAGSGFTPUAT/variables.tf
+++ AZMAGSGFTPUAT/variables.tf
@@ -1,36 +1,9 @@
 #AzureRM
-variable "subscription_id" {
-  description = "azure subscription id"
-  default     = "${var.subscription_id}"
-}
+variable "subscription_id" {}
 
-variable "tenant_id" {
-  description = "Directory ID "
-  default     = "${var.tenant_id}"
-}
+variable "azurerm_resource_group_name_machine" {}
+variable "azurerm_resource_group_name_vnet" {}
 
-variable "azurerm_resource_group" {
-  description = "Name of resource group"
-  default     = "AZMAGRGFTPUAT"
-}
-
-variable "location" {
-  description = "Location of resource group"
-  default     = "ukwest"
-}
-
-variable "azurerm_virtual_network" {
-  default = "AZMAGRGVNET1"
-}
-
-variable "azurerm_subnet" {
-  default = "AZMAGSNET11"
-}
-
-variable "admin_username" {
-  default = "${var.vm_username}"
-}
-
-variable "admin_password" {
-  default = "${var.vm_password}"
-}
+variable "location" {}
+variable "vnet_cidr" {}
+variable "subnet1_cidr" {}
