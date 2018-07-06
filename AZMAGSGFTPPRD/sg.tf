d8eb82954685462aa6d517eef8cb5582b7aa3f66
bpourian <b.pourian@hotmail.co.uk>
2018-07-06 13:38:25 +0100

    Created security group for PRD machine

---
+++ AZMAGSGFTPPRD/sg.tf
@@ -0,0 +1,29 @@
+resource "azurerm_network_security_group" "puat-sg" {
+    name                = "AZMAGSGFTPPRD"
+    location            = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.machine.name}"
+
+    security_rule {
+      name                       = "AllowFTP"
+      access                     = "Allow"
+      direction                  = "Inbound"
+      priority                   = "100"
+      protocol                   = "TCP"
+      destination_port_ranges    = ["21", "5000-5050"]
+      destination_address_prefix = "*"
+      source_port_range          = "*"
+      source_address_prefix      = "${var.source_ip_prefix}"
+    }
+
+    security_rule {
+      name                       = "AllowRDP"
+      access                     = "Allow"
+      direction                  = "Inbound"
+      priority                   = "110"
+      protocol                   = "TCP"
+      destination_port_range     = "3389"
+      destination_address_prefix = "*"
+      source_port_range          = "*"
+      source_address_prefix      = "${var.source_ip_prefix}"
+    }
+}
